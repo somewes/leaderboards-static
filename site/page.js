e35399cf69ccf875c65d336b5da9880ec437ec6b
jrajav <jrajav+git@gmail.com>
2014-03-23 16:51:27 -0500

    Filling out index with search box and game list

--- site/page.js
+++ site/page.js
@@ -1,6 +1,27 @@
-define(['Ractive', 'rv!./template'], function (Ractive, template) {
+define(['Ractive', 'rv!./template', 'data/games'],
+function (Ractive, template, games) {
+
+	var contains = function (str, search) {
+		var strLower = str.toLocaleLowerCase();
+		var searchLower = search.toLocaleLowerCase();
+
+		return strLower.indexOf(searchLower) !== -1;
+	};
+
+
 	var ractive = new Ractive({
 		el: 'main',
-		template: template
+		template: template,
+		data: {
+			games: games
+		}
 	});
+
+	ractive.observe('searchtext', function (searchtext) {
+		ractive.set('games', games.filter(function (game) {
+			// TODO: Fuzzy matching, abbreviation matching, first-letter abbreviation matching
+			return contains(game.name, searchtext);
+		}));
+	});
+
 });
