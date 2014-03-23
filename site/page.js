14e4238cf5be90e51304d45ebd838fc76738cff8
jrajav <jrajav+git@gmail.com>
2014-03-23 17:49:14 -0500

    Moving filter logic to template

--- site/page.js
+++ site/page.js
@@ -1,27 +1,20 @@
 define(['Ractive', 'rv!./template', 'data/games'],
 function (Ractive, template, games) {
-
-	var contains = function (str, search) {
-		var strLower = str.toLocaleLowerCase();
-		var searchLower = search.toLocaleLowerCase();
-
-		return strLower.indexOf(searchLower) !== -1;
-	};
-
-
 	var ractive = new Ractive({
 		el: 'main',
 		template: template,
 		data: {
-			games: games
+			games: games,
+			contains: function (str, search) {
+				// TODO: Fuzzy matching, abbreviation matching, first-letter abbreviation matching
+
+				if (typeof search === 'undefined') return true;
+
+				var strLower = str.toLocaleLowerCase();
+				var searchLower = search.toLocaleLowerCase();
+
+				return strLower.indexOf(searchLower) !== -1;
+			}
 		}
 	});
-
-	ractive.observe('searchtext', function (searchtext) {
-		ractive.set('games', games.filter(function (game) {
-			// TODO: Fuzzy matching, abbreviation matching, first-letter abbreviation matching
-			return contains(game.name, searchtext);
-		}));
-	});
-
 });
