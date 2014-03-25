74865de57ddfa1d61a1f3b60ecf1849a8fd68122
jrajav <jrajav@gmail.com>
2014-03-25 16:18:13 -0500

    Not much

--- site/config.js
+++ site/config.js
@@ -1,10 +1,10 @@
 require.config({
   baseUrl: '/',
   paths: {
-    rv: "lib/requirejs-ractive/rv",
-    rvc: "lib/requirejs-ractive/rvc",
-    requirejs: "lib/requirejs/require",
-    "amd-loader": "lib/requirejs-ractive/amd-loader",
-    Ractive: "lib/Ractive/build/Ractive"
+    rv: 'lib/requirejs-ractive/rv',
+    rvc: 'lib/requirejs-ractive/rvc',
+    requirejs: 'lib/requirejs/require',
+    'amd-loader': 'lib/requirejs-ractive/amd-loader',
+    Ractive: 'lib/Ractive/build/Ractive'
   }
 });
