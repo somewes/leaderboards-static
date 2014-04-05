d67ef09acc10a1ddf9f99d73e106bc31492c0455
jrajav <jrajav+git@gmail.com>
2014-04-05 14:47:20 -0500

    Hooking submit up to API init

--- site/submit/page.js
+++ site/submit/page.js
@@ -1,39 +1,46 @@
-define(['lodash', 'Ractive', 'rv!./template', 'data/games/sm64', 'data/platforms'], function (_, Ractive, template, game, platforms) {
+define(['jquery', 'lodash', 'Ractive', 'rv!./template', 'leaderboards/api/games/sm64', 'leaderboards/api/platforms', 'components/tagvalueselect'], function ($, _, Ractive, template, game, platforms) {
+
+	// TEMPORARY until tag types added to server
+	_.each(game.tags, function (tag) {
+		tag.type = 'list';
+	});
+
 
 	var levenshtein = (function() {
-	        var row2 = [];
-	        return function(s1, s2) {
-	            if (s1 === s2) {
-	                return 0;
-	            } else {
-	                var s1_len = s1.length, s2_len = s2.length;
-	                if (s1_len && s2_len) {
-	                    var i1 = 0, i2 = 0, a, b, c, c2, row = row2;
-	                    while (i1 < s1_len)
-	                        row[i1] = ++i1;
-	                    while (i2 < s2_len) {
-	                        c2 = s2.charCodeAt(i2);
-	                        a = i2;
-	                        ++i2;
-	                        b = i2;
-	                        for (i1 = 0; i1 < s1_len; ++i1) {
-	                            c = a + (s1.charCodeAt(i1) === c2 ? 0 : 1);
-	                            a = row[i1];
-	                            b = b < a ? (b < c ? b + 1 : c) : (a < c ? a + 1 : c);
-	                            row[i1] = b;
-	                        }
-	                    }
-	                    return b;
-	                } else {
-	                    return s1_len + s2_len;
-	                }
-	            }
-	        };
+		var row2 = [];
+		return function(s1, s2) {
+		    if (s1 === s2) {
+		        return 0;
+		    } else {
+		        var s1_len = s1.length, s2_len = s2.length;
+		        if (s1_len && s2_len) {
+		            var i1 = 0, i2 = 0, a, b, c, c2, row = row2;
+		            while (i1 < s1_len)
+		                row[i1] = ++i1;
+		            while (i2 < s2_len) {
+		                c2 = s2.charCodeAt(i2);
+		                a = i2;
+		                ++i2;
+		                b = i2;
+		                for (i1 = 0; i1 < s1_len; ++i1) {
+		                    c = a + (s1.charCodeAt(i1) === c2 ? 0 : 1);
+		                    a = row[i1];
+		                    b = b < a ? (b < c ? b + 1 : c) : (a < c ? a + 1 : c);
+		                    row[i1] = b;
+		                }
+		            }
+		            return b;
+		        } else {
+		            return s1_len + s2_len;
+		        }
+		    }
+		};
 	})();
 
 	var parseTime = function (input) {
-		if (typeof input !== 'string')
+		if (typeof input !== 'string') {
 			return 0;
+		}
 
 		var ms = 0;
 
@@ -41,7 +48,8 @@ define(['lodash', 'Ractive', 'rv!./template', 'data/games/sm64', 'data/platforms
 			'hours': function (s) { return (_.parseInt(s) || 0) * 60 * 60 * 1000; },
 			'minutes': function (s) { return (_.parseInt(s) || 0) * 60 * 1000; },
 			'seconds': function (s) { return (_.parseInt(s) || 0) * 1000; },
-			'milliseconds': function (s) { return (_.parseInt(s) || 0); }
+			'milliseconds': function (s) { return (_.parseInt(s) || 0); },
+			'ms': function (s) { return (_.parseInt(s) || 0); }
 		};
 
 		var match = /^\s*((\d{1,2}):)?(\d{1,2})?:(\d\d)?(.(\d{1,3}))?\s*$/.exec(input);
@@ -59,9 +67,8 @@ define(['lodash', 'Ractive', 'rv!./template', 'data/games/sm64', 'data/platforms
 				_.each(numbers, function (number, index) {
 					var word = words[index];
 					if (word) {
-						word = word.toLowerCase();
 						ms += (_.min(tomilliseconds, function (fn, key) {
-							return levenshtein(key.substring(0, word.length), word);
+							return levenshtein(key.substring(0, word.length), word.toLowerCase());
 						}))(number);
 					}
 				});
@@ -71,10 +78,17 @@ define(['lodash', 'Ractive', 'rv!./template', 'data/games/sm64', 'data/platforms
 		return ms;
 	};
 
+	var tags = _.clone(game.tags, true);
+
 	var data = {
+		_: _,
 		game: game,
-		platforms: platforms,
-		time: null
+		platforms: _.filter(platforms, function (p) { return _.contains(game.platforms, p.id); }),
+		// Selected values for submission
+		timedtags: _.where(tags, {timed: true}),
+		untimedtags: _.where(tags, {untimed: true}),
+		time: null,
+		platform: null
 	};
 
 	data.formatTime = function (time) {
@@ -83,7 +97,7 @@ define(['lodash', 'Ractive', 'rv!./template', 'data/games/sm64', 'data/platforms
 		var seconds = Math.floor(time / 1000) % 60;
 		var milliseconds = time % 1000;
 
-		return 'Hours: ' + hours + ', Minutes: ' + minutes + ', Seconds: ' + seconds + ', Milliseconds: ' + milliseconds;
+		return hours + 'h ' + minutes + 'm ' + seconds + 's ' + milliseconds + 'ms';
 	};
 
 	var ractive = new Ractive({
@@ -97,4 +111,24 @@ define(['lodash', 'Ractive', 'rv!./template', 'data/games/sm64', 'data/platforms
 		ractive.set('time', parseTime(timeinput));
 	});
 
+	ractive.on('submit', function (event) {
+		event.original.preventDefault();
+
+		ractive.set('info', 'Submitting...');
+
+		$.post('/leaderboards/api/speedrun/', {
+			user: 2,
+			game: game.id,
+			time: ractive.get('time'),
+			video: ractive.get('video'),
+			comments: ractive.get('comments'),
+			tags: [],
+			username: 'speedrunner',
+			api_key: 'a87e33657f5f6796bb3a57ecc213f712edf02088'
+		}).done(function (response) {
+			ractive.set('info', 'Success!');
+		}).fail(function (response) {
+			ractive.set('info', 'Error: ' + JSON.stringify(response));
+		});
+	});
 });
