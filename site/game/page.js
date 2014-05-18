e63910b73cfad0a63d6409272380a080cfb3b314
jrajav <jrajav+git@gmail.com>
2014-05-18 17:43:47 -0500

    Restructure data, add filter length limit

--- site/game/page.js
+++ site/game/page.js
@@ -4,7 +4,7 @@ define(function (require) {
 		moment   = require('moment'),
 		Ractive  = require('Ractive'),
 
-	    template = require('rv!./template'),
+		template = require('rv!./template'),
 
 		game     = require('game'),
 
@@ -18,81 +18,90 @@ define(function (require) {
 	}
 
 
-	function filterRuns(runs, filter) { // block filter
-		if (!filter) {
-			return
-		}
+	var data = {
 
-		var tagblocks = _.filter(filter.tagblocks, function (tagblock) {
-			return tagblock.tagvalues.length > 0
-		})
+		game: game,
 
-		return _.filter(runs, function (run) {
-			return _.all(tagblocks, function (tagblock) {
-				var values = tagblock.tagvalues;
-				return values.length > 0 && _.any(values, function (value) {
-					return _.contains(run.tagvalues, value.id)
+		platforms: hashByProp(game.platforms, 'id'),
+
+		tags: hashByProp(game.tags, 'id'),
+
+		tagvalues: hashByProp(game.tagvalues, 'id'),
+
+		filterRuns: function filterRuns(runs, filter) { // block filter
+			if (!filter) {
+				return
+			}
+
+			var tagblocks = _.filter(filter.tagblocks, function (tagblock) {
+				return tagblock.tagvalues.length > 0
+			})
+
+			return _.filter(runs, function (run) {
+				return _.all(tagblocks, function (tagblock) {
+					var values = tagblock.tagvalues;
+					return values.length > 0 && _.any(values, function (value) {
+						return _.contains(run.tagvalues, value.id)
+					})
+				})
+			})
+		},
+
+		limit: function limit(list, length) {
+			return _.first(list, length)
+		},
+
+		filters: _.map(game.filters, function (filter) {
+			return {
+				id: filter.id,
+				name: filter.name,
+				tagblocks: _.map(game.tags, function (tag) {
+					return {
+						tag: tag,
+						tagvalues: _.map(_.intersection(filter.tagvalues, tag.tagvalues), function (tagvalueid) {
+							return _.findWhere(game.tagvalues, { id: tagvalueid })
+						})
+					}
+				})
+			}
+		}),
+
+		augmentedRuns: _.map(game.speedruns, function (run) {
+			return _.assign(_.clone(run), {
+				tagblocks: _.map(game.tags, function (tag) {
+					return {
+						tag: tag,
+						tagvalues: _.map(_.intersection(run.tagvalues, tag.tagvalues), function (tagvalueid) {
+							return _.findWhere(game.tagvalues, { id: tagvalueid })
+						})
+					}
 				})
 			})
 		})
 	}
 
 
-	var blockFilters = _.map(game.filters, function (filter) {
-		return {
-			id: filter.id,
-			name: filter.name,
-			tagblocks: _.map(game.tags, function (tag) {
-				return {
-					tag: tag,
-					tagvalues: _.map(_.intersection(filter.tagvalues, tag.tagvalues), function (tagvalueid) {
-						return _.findWhere(game.tagvalues, { id: tagvalueid })
-					})
-				}
-			})
-		}
-	})
-
-
-	var augmentedRuns = _.map(game.speedruns, function (run) {
-		return _.assign(_.clone(run), {
-			tagblocks: _.map(game.tags, function (tag) {
-				return {
-					tag: tag,
-					tagvalues: _.map(_.intersection(run.tagvalues, tag.tagvalues), function (tagvalueid) {
-						return _.findWhere(game.tagvalues, { id: tagvalueid })
-					})
-				}
-			})
-		})
-	})
+	var components = {
+		tagblock: R_tag,
+		runblock: R_run
+	}
 
 
 	var ractive = new Ractive({
 		el: 'main',
 		template: template,
 		debug: true,
-		data: {
-			game: game,
-			augmentedRuns: augmentedRuns,
-			platforms: hashByProp(game.platforms, 'id'),
-			tags: hashByProp(game.tags, 'id'),
-			tagvalues: hashByProp(game.tagvalues, 'id'),
-			filters: blockFilters,
-			filterRuns: filterRuns
-		},
-		components: {
-			tagblock: R_tag,
-			runblock: R_run
-		}
+		data: data,
+		components: components
 	})
+
 
 	ractive.on('change-filter-game', function (event) {
 		ractive.set('filter', undefined)
 	})
 
 	ractive.on('change-filter', function (event, filterid) {
-		ractive.set('filter', _.findWhere(blockFilters, { id: filterid }))
+		ractive.set('filter', _.findWhere(data.filters, { id: filterid }))
 	})
 
 })
