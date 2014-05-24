d69c26ec2bf82a257a4f3c75f46f4659c0362f5c
jrajav <jrajav@gmail.com>
2014-05-23 21:55:12 -0500

    Editable selection for tag component

--- site/components/tag.js
+++ site/components/tag.js
@@ -11,6 +11,15 @@ define(function (require) {
 		debug: true,
 		data: {
 			isEmpty: _.isEmpty
+		},
+		init: function () {
+			var self = this
+			self.on('toggle', function (event) {
+				var tagvalue = event.context;
+				var keypath = 'selectedvalues[' + tagvalue.id +']'
+				var selected = self.get(keypath)
+				self.set(keypath, selected ? undefined : tagvalue)
+			})
 		}
 	})
 
